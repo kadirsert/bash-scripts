6f662ad6a456179908799ed14d4e2dcfb5cd18c3
Kadir Sert <kadirsert80@gmail.com>
2020-06-16 15:21:34 +0300

    Create list_docker_networks.sh

---
+++ list_docker_networks.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+red="\033[0;31m"
+green="\e[32m"
+bold="\033[1m"
+def="\033[0m"
+
+for net_name in $(docker network ls |tail -n +2 |awk '{print $2}')
+do printf '%-6b%20b\n' "${red}${bold}Name:" "${net_name}${def}"
+  echo "---------------------"
+  net_driver=$(docker network inspect --format='{{ .Driver }}' ${net_name})
+  echo -e "${green}Driver:${def} ${net_driver}"
+  if [ "${net_driver}" == "bridge" ]; then
+    echo -en "${green}Binding IP:${def} "
+    docker network inspect --format='{{ index .Options "com.docker.network.bridge.host_binding_ipv4" }}' ${net_name}
+    echo -en "${green}Bridge Name:${def} "
+    docker network inspect --format='{{ index .Options "com.docker.network.bridge.name" }}' ${net_name}
+  fi
+  echo -e "${green}Containers:${def}"
+  docker network inspect -f '{{ range .Containers }}{{println .Name}}{{ end }}' ${net_name}
+  echo
+done
