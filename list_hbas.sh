23baf1536c78573627f86ac74561c884201e2180
ksert <kadir.sert@yurticikargo.com>
2021-08-25 15:09:00 +0300

    list_hbas.sh print supported speeds

--- list_hbas.sh
+++ list_hbas.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 
-host_name=$(hostname -s)
 if [ -d "/sys/class/fc_host" ]; then
   cd /sys/class/fc_host/
-  printf '%-20b%-12b%-12b%-30b\n' "Hostname:" "Device:" "Port State:" "Port Name:"
+  printf '%-12b%-16b%-30b%-30b\n' "Device" "Port State" "Port Name" "Supported Speeds"
   for i in $(ls)
     do
       device=$i
       p_n=$(cat $i/port_name)
       port_name="${p_n:2:2}:${p_n:4:2}:${p_n:6:2}:${p_n:8:2}:${p_n:10:2}:${p_n:12:2}:${p_n:14:2}:${p_n:16:2}"
       port_state=$(cat $i/port_state)
-      printf '%-20b%-12b%-12b%-30b\n' "${host_name}" "${device}" "${port_state}" "${port_name}"
+      supported_speeds=$(cat $i/supported_speeds)
+      #echo "$host_name-$device-$port_state-$port_name"
+      printf '%-12b%-16b%-30b%-30b\n' "${device}" "${port_state}" "${port_name}" "${supported_speeds}"
     done
 else
   echo "fc_host directory does not exist!!!"
 fi
+
