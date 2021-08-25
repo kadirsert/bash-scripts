7d994cd2d63e3b4426ab517496b9e34a1b5342e8
ksert <kadir.sert@yurticikargo.com>
2021-08-25 11:24:42 +0300

    list_hbas.sh pretty print

--- list_hbas.sh
+++ list_hbas.sh
@@ -3,13 +3,14 @@
 host_name=$(hostname -s)
 if [ -d "/sys/class/fc_host" ]; then
   cd /sys/class/fc_host/
+  printf '%-20b%-12b%-12b%-30b\n' "Hostname:" "Device:" "Port State:" "Port Name:"
   for i in $(ls)
     do
       device=$i
       p_n=$(cat $i/port_name)
       port_name="${p_n:2:2}:${p_n:4:2}:${p_n:6:2}:${p_n:8:2}:${p_n:10:2}:${p_n:12:2}:${p_n:14:2}:${p_n:16:2}"
       port_state=$(cat $i/port_state)
-      echo "$host_name;$device;$port_state;$port_name"
+      printf '%-20b%-12b%-12b%-30b\n' "${host_name}" "${device}" "${port_state}" "${port_name}"
     done
 else
   echo "fc_host directory does not exist!!!"
